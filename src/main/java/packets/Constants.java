25f9ea6d6a8aff7b5dfce9b224c332e718a30534
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 18:23:17 +0300

    update MIN_LENGTH constant

--- src/main/java/packets/Constants.java
+++ src/main/java/packets/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
     public static final int OFFSET_LEN = 10;
     public static final int OFFSET_CRC = 14;
     public static final int OFFSET_MSG = 16;
-    public static final int MIN_LENGTH = 18;
     public static final int MSG_OFFSET_C_TYPE = 0;
     public static final int MSG_OFFSET_B_USER_ID = 4;
     public static final int MSG_OFFSET_MESSAGE = 8;
+    public static final int MIN_LENGTH = OFFSET_MSG + MSG_OFFSET_MESSAGE + Short.BYTES;
 }
