44ef763c2e1875580ca0116e246876b9e778a7ab
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 18:14:53 +0300

    add Cipher to wrapper

--- src/main/java/packets/abstractions/MessageWrapper.java
+++ src/main/java/packets/abstractions/MessageWrapper.java
@@ -1,5 +1,7 @@
 package packets.abstractions;
 
+import javax.crypto.Cipher;
+
 public interface MessageWrapper {
-    byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId);
+    byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId, Cipher cipher);
 }
--- src/main/java/packets/implementations/MessageWrapperImpl.java
+++ src/main/java/packets/implementations/MessageWrapperImpl.java
@@ -2,9 +2,11 @@ package packets.implementations;
 
 import packets.abstractions.MessageWrapper;
 
+import javax.crypto.Cipher;
+
 public class MessageWrapperImpl implements MessageWrapper {
     @Override
-    public byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId) {
+    public byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId, Cipher cipher) {
         return new byte[0];
     }
 }
