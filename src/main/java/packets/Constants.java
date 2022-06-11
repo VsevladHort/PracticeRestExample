a739b66ce06d3bfa16da043fc0b9056101bbe64e
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 12:56:27 +0300

    add offsets to Constants

--- src/main/java/packets/Constants.java
+++ src/main/java/packets/Constants.java
@@ -5,4 +5,13 @@ public class Constants {
     }
 
     public static final int MAGIC = 0x13;
+    public static final int OFFSET_MAGIC = 0;
+    public static final int OFFSET_SRC = 1;
+    public static final int OFFSET_PKT_ID = 2;
+    public static final int OFFSET_LEN = 10;
+    public static final int OFFSET_CRC = 14;
+    public static final int OFFSET_MSG = 16;
+    public static final int MSG_OFFSET_C_TYPE = 0;
+    public static final int MSG_OFFSET_B_USER_ID = 4;
+    public static final int MSG_OFFSET_MESSAGE = 8;
 }
