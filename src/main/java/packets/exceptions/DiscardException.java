82b05454feb05574368011fbe47fc141d1d35732
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 14:55:31 +0300

    add DiscardException constructor

--- src/main/java/packets/exceptions/DiscardException.java
+++ src/main/java/packets/exceptions/DiscardException.java
@@ -3,4 +3,7 @@ package packets.exceptions;
 import java.io.IOException;
 
 public class DiscardException extends IOException {
+    public DiscardException(String msg) {
+        super(msg);
+    }
 }
