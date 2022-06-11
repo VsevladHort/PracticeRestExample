c78e599f4d3ddb1dadec5789697010ed81c68d8b
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 17:52:26 +0300

    update MessageWrapper contract

--- src/main/java/packets/abstractions/MessageWrapper.java
+++ src/main/java/packets/abstractions/MessageWrapper.java
@@ -1,4 +1,5 @@
 package packets.abstractions;
 
 public interface MessageWrapper {
+    byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId);
 }
--- src/main/java/packets/implementations/MessageWrapperImpl.java
+++ src/main/java/packets/implementations/MessageWrapperImpl.java
@@ -3,4 +3,8 @@ package packets.implementations;
 import packets.abstractions.MessageWrapper;
 
 public class MessageWrapperImpl implements MessageWrapper {
+    @Override
+    public byte[] wrap(byte[] message, byte bSrc, byte bPktId, byte cType, byte bUserId) {
+        return new byte[0];
+    }
 }
