8f745471eb71829509351900d2afea6e987e4469
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 13:06:48 +0300

    Revert "change Constants visibility to package-level"
    This reverts commit 50c7743835f6047d62ceabfa5065bdfe2dd07ddf.

--- src/main/java/packets/Constants.java
+++ src/main/java/packets/Constants.java
@@ -1,17 +1,17 @@
 package packets;
 
-class Constants {
+public class Constants {
     private Constants() {
     }
 
-    static final int MAGIC = 0x13;
-    static final int OFFSET_MAGIC = 0;
-    static final int OFFSET_SRC = 1;
-    static final int OFFSET_PKT_ID = 2;
-    static final int OFFSET_LEN = 10;
-    static final int OFFSET_CRC = 14;
-    static final int OFFSET_MSG = 16;
-    static final int MSG_OFFSET_C_TYPE = 0;
-    static final int MSG_OFFSET_B_USER_ID = 4;
-    static final int MSG_OFFSET_MESSAGE = 8;
+    public static final int MAGIC = 0x13;
+    public static final int OFFSET_MAGIC = 0;
+    public static final int OFFSET_SRC = 1;
+    public static final int OFFSET_PKT_ID = 2;
+    public static final int OFFSET_LEN = 10;
+    public static final int OFFSET_CRC = 14;
+    public static final int OFFSET_MSG = 16;
+    public static final int MSG_OFFSET_C_TYPE = 0;
+    public static final int MSG_OFFSET_B_USER_ID = 4;
+    public static final int MSG_OFFSET_MESSAGE = 8;
 }
