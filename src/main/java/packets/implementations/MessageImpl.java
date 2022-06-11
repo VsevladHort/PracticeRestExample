6d5f3f0a52248c9594be3520cf03d91ad68048b6
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 13:10:59 +0300

    create constructor for MessageImpl

--- src/main/java/packets/implementations/MessageImpl.java
+++ src/main/java/packets/implementations/MessageImpl.java
@@ -1,14 +1,20 @@
 package packets.implementations;
 
+import packets.Constants;
 import packets.abstractions.Message;
+
+import java.nio.ByteBuffer;
 
 public class MessageImpl implements Message {
     private int cType;
     private int bUserId;
     private byte[] message;
 
-    public MessageImpl(byte[] bytes, int offsetToMessage) {
-
+    public MessageImpl(ByteBuffer bytes, int wLen) {
+        cType = bytes.getInt(Constants.OFFSET_MSG + Constants.MSG_OFFSET_C_TYPE);
+        bUserId = bytes.getInt(Constants.OFFSET_MSG + Constants.MSG_OFFSET_B_USER_ID);
+        message = new byte[wLen];
+        bytes.get(Constants.OFFSET_MSG + Constants.MSG_OFFSET_MESSAGE, message);
     }
 
     @Override
