ec77d93747386825a8859069895cc15b1dab4fcd
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-11 17:28:22 +0300

    update Packet interface contract

--- src/main/java/packets/abstractions/Packet.java
+++ src/main/java/packets/abstractions/Packet.java
@@ -1,4 +1,17 @@
 package packets.abstractions;
 
 public interface Packet {
+    byte getBMagic();
+
+    byte getBSrc();
+
+    long getBPktId();
+
+    int getWLen();
+
+    short getWCrc16n1();
+
+    Message getMessage();
+
+    short getWCrc16n2();
 }
