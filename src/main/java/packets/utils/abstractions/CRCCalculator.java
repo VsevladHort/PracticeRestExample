c163f47f584946295c7c281135ab226f83f65c66
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-12 15:16:40 +0300

    overload CRCCalculator calculate to handle lists

--- src/main/java/packets/utils/abstractions/CRCCalculator.java
+++ src/main/java/packets/utils/abstractions/CRCCalculator.java
@@ -1,8 +1,5 @@
 package packets.utils.abstractions;
 
-import java.util.List;
-
 public interface CRCCalculator {
     short calculate(byte[] bytes, int start, int end);
-    short calculate(List<Byte> bytes, int start, int end);
 }
