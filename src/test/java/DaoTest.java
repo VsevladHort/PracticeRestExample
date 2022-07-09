173933883f62d1d696ced5f6a16096f731e88bcc
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-07-09 23:24:52 +0300

    add tests to DBService

---
+++ src/test/java/DaoTest.java
@@ -0,0 +1,226 @@
+import dao.CriteriaGood;
+import dao.CriteriaGoodGroup;
+import dao.DBService;
+import dao.Dao;
+import dao.exceptions.DaoWrapperException;
+import entities.Good;
+import entities.GoodGroup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+class DaoTest {
+    private static final String TEST_DB = "test";
+    private Dao dao;
+
+    @BeforeEach
+    void beforeEach() throws DaoWrapperException {
+        var file = new File(TEST_DB);
+        if (file.exists())
+            file.delete();
+        dao = new DBService(TEST_DB);
+    }
+
+    @Test
+    void testGroupInsertion() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        dao.createGroup(group);
+        var gottenGroup = dao.getGroup("1");
+        Assertions.assertEquals(group.getName(), gottenGroup.getName());
+        Assertions.assertEquals(group.getDescription(), gottenGroup.getDescription());
+    }
+
+    @Test
+    void testGroupUpdate() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        dao.createGroup(group);
+        group.setDescription("NewDesc");
+        dao.updateGroup(group);
+        var gottenGroup = dao.getGroup("1");
+        Assertions.assertEquals(group.getDescription(), gottenGroup.getDescription());
+    }
+
+    @Test
+    void testGroupDelete() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        dao.createGroup(group);
+        group.setDescription("NewDesc");
+        dao.deleteGoodGroup(group.getName());
+        var gottenGroup = dao.getGroup("1");
+        Assertions.assertNull(gottenGroup);
+    }
+
+    @Test
+    void testGroupListByName() throws DaoWrapperException {
+        var group1 = new GoodGroup("1", "c");
+        var group2 = new GoodGroup("2", "b");
+        var group3 = new GoodGroup("3", "a");
+        dao.createGroup(group1);
+        dao.createGroup(group2);
+        dao.createGroup(group3);
+        var gottenGroupsName = dao.getGroups(2, CriteriaGoodGroup.NAME);
+        var expectedOrderName = new ArrayList<GoodGroup>();
+        expectedOrderName.add(group3);
+        expectedOrderName.add(group2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedOrderName.get(i).getName(), gottenGroupsName.get(i).getName());
+        }
+    }
+
+    @Test
+    void testGroupListByDescription() throws DaoWrapperException {
+        var group1 = new GoodGroup("1", "c");
+        var group2 = new GoodGroup("2", "b");
+        var group3 = new GoodGroup("3", "a");
+        dao.createGroup(group1);
+        dao.createGroup(group2);
+        dao.createGroup(group3);
+        var gottenGroupsDescription = dao.getGroups(2, CriteriaGoodGroup.DESCRIPTION);
+        var expectedOrderDescription = new ArrayList<GoodGroup>();
+        expectedOrderDescription.add(group1);
+        expectedOrderDescription.add(group2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedOrderDescription.get(i).getDescription(), gottenGroupsDescription.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodInsertion() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good = new Good("good");
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good);
+        var gottenGood = dao.getGood("1", "good");
+        Assertions.assertEquals(good.getName(), gottenGood.getName());
+    }
+
+    @Test
+    void testGoodListByName() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good1 = new Good("1", "a", 3, "c", 1);
+        var good2 = new Good("2", "b", 2, "b", 2);
+        var good3 = new Good("3", "c", 1, "a", 3);
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good1);
+        dao.createGood(group.getName(), good2);
+        dao.createGood(group.getName(), good3);
+        var gottenGoods = dao.getGoods(group.getName(), 2, CriteriaGood.NAME);
+        var expectedGottenGoods = new ArrayList<Good>();
+        expectedGottenGoods.add(good3);
+        expectedGottenGoods.add(good2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedGottenGoods.get(i).getDescription(), gottenGoods.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodListByDescription() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good1 = new Good("1", "a", 3, "c", 1);
+        var good2 = new Good("2", "b", 2, "b", 2);
+        var good3 = new Good("3", "c", 1, "a", 3);
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good1);
+        dao.createGood(group.getName(), good2);
+        dao.createGood(group.getName(), good3);
+        var gottenGoods = dao.getGoods(group.getName(), 2, CriteriaGood.DESCRIPTION);
+        var expectedGottenGoods = new ArrayList<Good>();
+        expectedGottenGoods.add(good3);
+        expectedGottenGoods.add(good2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedGottenGoods.get(i).getDescription(), gottenGoods.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodListByPrice() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good1 = new Good("1", "a", 3, "c", 1);
+        var good2 = new Good("2", "b", 2, "b", 2);
+        var good3 = new Good("3", "c", 1, "a", 3);
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good1);
+        dao.createGood(group.getName(), good2);
+        dao.createGood(group.getName(), good3);
+        var gottenGoods = dao.getGoods(group.getName(), 2, CriteriaGood.PRICE);
+        var expectedGottenGoods = new ArrayList<Good>();
+        expectedGottenGoods.add(good1);
+        expectedGottenGoods.add(good2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedGottenGoods.get(i).getDescription(), gottenGoods.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodListByProducer() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good1 = new Good("1", "a", 3, "c", 1);
+        var good2 = new Good("2", "b", 2, "b", 2);
+        var good3 = new Good("3", "c", 1, "a", 3);
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good1);
+        dao.createGood(group.getName(), good2);
+        dao.createGood(group.getName(), good3);
+        var gottenGoods = dao.getGoods(group.getName(), 2, CriteriaGood.PRODUCER);
+        var expectedGottenGoods = new ArrayList<Good>();
+        expectedGottenGoods.add(good1);
+        expectedGottenGoods.add(good2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedGottenGoods.get(i).getDescription(), gottenGoods.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodListByAmount() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good1 = new Good("1", "a", 3, "c", 1);
+        var good2 = new Good("2", "b", 2, "b", 2);
+        var good3 = new Good("3", "c", 1, "a", 3);
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good1);
+        dao.createGood(group.getName(), good2);
+        dao.createGood(group.getName(), good3);
+        var gottenGoods = dao.getGoods(group.getName(), 2, CriteriaGood.AMOUNT);
+        var expectedGottenGoods = new ArrayList<Good>();
+        expectedGottenGoods.add(good3);
+        expectedGottenGoods.add(good2);
+        for (int i = 0; i < 2; i++) {
+            Assertions.assertEquals(expectedGottenGoods.get(i).getDescription(), gottenGoods.get(i).getDescription());
+        }
+    }
+
+    @Test
+    void testGoodUpdate() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good = new Good("good");
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good);
+        good.setPrice(10);
+        dao.updateGood(group.getName(), good);
+        var gottenGood = dao.getGood("1", "good");
+        Assertions.assertEquals(good.getPrice(), gottenGood.getPrice());
+    }
+
+    @Test
+    void testGoodDelete() throws DaoWrapperException {
+        var group = new GoodGroup("1", "desc");
+        var good = new Good("good");
+        dao.createGroup(group);
+        dao.createGood(group.getName(), good);
+        dao.deleteGood(group.getName(), good.getName());
+        var gottenGood = dao.getGood("1", "good");
+        Assertions.assertNull(gottenGood);
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        if (dao instanceof DBService)
+            ((DBService) dao).close();
+        dao = null;
+    }
+}
